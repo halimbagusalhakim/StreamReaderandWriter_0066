007c8bdb9bd2cee7571ff9d2380a00e0cff5b6f1
halimbagusalhakim <halsat132@gmail.com>
2024-06-03 14:27:30 +0700

    menulis dan memasukan nilai dari 'baris' ke dalam file

--- ioFile.cpp
+++ ioFile.cpp
@@ -21,6 +21,9 @@ int main() {
         getline(cin, baris);
         //loop akan berhenti jika anda memasukan karakter q
         if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;  
+             
 
     }
 
