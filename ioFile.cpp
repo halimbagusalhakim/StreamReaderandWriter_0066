6e1f649973a77310962dc9d78ae349ebba10fb1e
halimbagusalhakim <halsat132@gmail.com>
2024-06-03 14:33:03 +0700

    melakukan perulangan setiap baris dan menampilkan baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -31,4 +31,16 @@ int main() {
     ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open()) {
+        
+        //melakukan perulaangan setiap baris
+        while (getline(infile, baris)){
+            
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+    }
 }
