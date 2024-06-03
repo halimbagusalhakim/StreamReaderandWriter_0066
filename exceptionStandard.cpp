e50c8a0074b86c5312ee70b507832cc1fb5c0124
halimbagusalhakim <halsat132@gmail.com>
2024-06-03 15:10:17 +0700

    membuat try func

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -3,3 +3,16 @@
 //untuk obyek exception yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+    
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {8, 4, 2};
+        //pesan array integer 3 elemen
+        cout <<data.at(5) << endl;
+
+    }
+    
+}
