854494477313d4c43be4f0e86ae199efa9676a9e
halimbagusalhakim <halsat132@gmail.com>
2024-06-03 14:40:41 +0700

    membuat variable, menginput nama file dan membuka file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,3 +3,14 @@
 #include <string>
 using namespace std;
 
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    outfile.open(NamaFile + ".txt", ios::out);
+}
